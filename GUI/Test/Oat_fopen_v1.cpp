64f33e7b1d3066c05fbb9ad267d493256affee49
GunTH13 <it58070031@office365.it.kmitl.ac.th>
2016-04-10 00:04:41 +0700

    Revert "Revert "GUI Function""
    This reverts commit 8f65b8f3c65f0c0bacb7ec7342e170740cf536bb.

---
+++ GUI/Test/Oat_fopen_v1.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h> //"C:\\Users\\oat_t\\Desktop\\work\\AES proj test\\test.txt"
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	FILE *f_ptr;
+	char text[50], path[100], new_text[50];
+	
+	/*strcpy(path, "C:\\Users\\oat_t\\Desktop\\work\\AES proj test\\test.txt");*/ //reserve path!
+	
+	printf("Drag file here: ");
+	scanf("\"%[^\"]\"", path);
+	
+	f_ptr = fopen(path, "r+t");
+	/* open file, mode > r = read exist file, w = create new file, +t = can put new text */
+	
+	if (!f_ptr) /* Or use f_ptr == NULL */
+	{
+		printf("Can't open file.\n");
+		printf("This is your path: %s", path);
+		exit(0);
+	}
+	
+	printf("\nThis is your path: %s", path);
+	
+	/*--------------------------------*/ /*bring text from ptr to variable*/
+	fscanf(f_ptr, "%[^\n]", text);
+	printf("%s", text);
+	//fgets(text,50,f_ptr);
+	/*--------------------------------*/ /*that fscanf and fgets is the same*/
+	
+	//printf("\nEnter your text: ");
+	//scanf("%s", new_text);
+	
+	/*--------------------------------*/ /* put the text to file */
+	//fputs(new_text, f_ptr); 
+	//fprintf(f_ptr, "%s", new_text);
+	/*--------------------------------*/ /*that fprintf and fputs is the same*/
+	
+	printf("\nPut new text Success!");
+	
+	
+}
